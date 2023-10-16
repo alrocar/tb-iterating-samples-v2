49c83ca6f8178ae0b64088b24f882310a15a61a5
alrocar <alrocar@tinybird.co>
2023-10-16 20:49:12 +0200

    sleep

--- deploy/0.0.1/ci-deploy.sh
+++ deploy/0.0.1/ci-deploy.sh
@@ -1,3 +1,4 @@
 tb datasource append analytics_events datasources/fixtures/analytics_events_errors.ndjson
 tb push pipes/analytics_events_quarantine_to_final.pipe
 tb pipe copy run analytics_events_quarantine_to_final --wait --yes
+sleep 10
