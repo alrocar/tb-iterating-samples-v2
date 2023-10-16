0b3fd0219cfa5091d26acbe6a31bf438a3362ca1
alrocar <alrocar@tinybird.co>
2023-10-16 20:25:07 +0200

    run copy

--- deploy/0.0.1/ci-deploy.sh
+++ deploy/0.0.1/ci-deploy.sh
@@ -1,2 +1,3 @@
 tb deploy --populate --fixtures
 tb datasource append analytics_events datasources/fixtures/analytics_events_errors.ndjson
+tb pipe copy run analytics_events_quarantine_to_final --wait --yes
